f16a8c76dd6c7a85b45155f9249b77a03f460abb
Crowdbotics <team@crowdbotics.com>
2020-10-26 16:55:25 +0000

    Update models

--- backend/gambot_21972/wsgi.py
+++ backend/gambot_21972/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gambot_21972.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gambot_21972.settings")
 
 application = get_wsgi_application()
